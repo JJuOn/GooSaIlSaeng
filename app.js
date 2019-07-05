481d619f5675bfaff4b6f5fd33bff5323ca5aca3
BambooKim <rlaqjarn1008@khu.ac.kr>
2019-07-05 17:31:22 +0900

    SHA512 Encryption with Crypto Module

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const crypto = require('crypto')
-
 require('dotenv').config()
 
 const app = express()
@@ -16,24 +15,8 @@ const db = mysql.createConnection({
 })
 db.connect()
 
-/*db.query("SHOW TABLES", (err, results) => {
-    console.log(results)
-})*/
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-
-//app.use(express.static(`${__dirname}/login`))
-
-/*app.use((req, res) => {
-    const userId = req.body.userId
-    const password = req.body.password
-
-    res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
-    res.write(`<div><p>${userId}</p></div>`)
-    res.write(`<div><p>${password}</p></div>`)
-    res.end(JSON.stringify(req.body, null, 2));
-})*/
 
 app.get('/', (req, res) => {
     res.write(`
@@ -65,22 +48,67 @@ app.post('/login', (req, res) => {
     const password = req.body.password
     
     if (status === 'student') {
-        db.query(`SELECT * FROM 학생 where 아이디=?`, [userId], (err, results) => {
+        db.query(`SELECT * FROM Student where UserId=?`, [userId], (err, results) => {
             if (err) throw err;
-            if (!results[0]) {
+            if (!results[0]) {      // 없는 아이디
+                console.log(`${userId} Id Failed at ${new Date()}`)
                 res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
-                res.write(`ID doesn't exist`)
+                res.write(`존재하지 않는 아이디입니다.`)
+                res.write(`<div><p><a href="/login">Login</a></p></div>`)
                 res.write(`<div><p><a href="/">HOME</a></p></div>`)
                 res.end()
             } else {
-                res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
-                res.write(`Exist`)
-                res.write(`<div><p><a href="/">HOME</a></p></div>`)
-                res.end()
+                let user = results[0]
+                crypto.pbkdf2(password, user.salt, Number(process.env.CRYPTO_ITERATION), 64, 'sha512', (err, derivedKey) => {
+                    if (err) throw err
+                    if (derivedKey.toString('base64') === user.Password) {
+                        console.log(`${results[0].UserId} Login Success at ${new Date()}`)
+                        res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                        res.write("Login Success")
+                        res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                        res.end()
+                    } else {
+                        console.log(`${results[0].UserId} Password Failed at ${new Date()}`)
+                        res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                        res.write("Please check your password.")
+                        res.write(`<div><p><a href="/login">Login</a></p></div>`)
+                        res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                        res.end()
+                    }
+                })
             }
         })
     } else if (status === 'faculty') {
-        
+        db.query(`SELECT * FROM Faculty where UserId=?`, [userId], (err, results) => {
+            if (err) throw err;
+            if (!results[0]) {      // 없는 아이디
+                console.log(`${userId} Id Failed at ${new Date()}`)
+                res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                res.write(`존재하지 않는 아이디입니다.`)
+                res.write(`<div><p><a href="/login">Login</a></p></div>`)
+                res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                res.end()
+            } else {
+                let user = results[0]
+                crypto.pbkdf2(password, user.salt, Number(process.env.CRYPTO_ITERATION), 64, 'sha512', (err, derivedKey) => {
+                    if (err) throw err
+                    if (derivedKey.toString('base64') === user.Password) {
+                        console.log(`${results[0].UserId} Login Success at ${new Date()}`)
+                        res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                        res.write("Login Success")
+                        res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                        res.end()
+                    } else {
+                        console.log(`${results[0].UserId} Password Failed at ${new Date()}`)
+                        res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                        res.write("Please check your password.")
+                        res.write(`<div><p><a href="/login">Login</a></p></div>`)
+                        res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                        res.end()
+                    }
+                })
+            }
+        })
     }
 })
 
@@ -100,22 +128,60 @@ app.post('/signup', (req, res) => {
     const password = req.body.password
 
     if (status === 'student') {
-        db.query(`INSERT INTO 학생 (학번, 이름, 아이디, 비밀번호, created) VALUES (?, ?, ?, ?, NOW())`, [numId, name, userId, password], (err, result) => {
-            if (err) throw err
-            res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
-            res.write(`<p>처리되었습니다.</p>`)
-            res.write(`<p><a href="/">HOME</a></p>`)
-            res.write(`<p><a href="login">Login</a></p>`)
-            res.end()
+        db.query(`SELECT * FROM Student where NumId=? || UserId=?`, [numId, userId], (err, results) => {
+            if (err) throw err;
+            if (results[0]) {       // 이미 존재하는 학번 또는 아이디
+                res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                res.write(`입력하신 학번 또는 아이디로 가입된 계정이 이미 존재합니다.`)
+                res.write(`<div><p><a href="/login">Login</a></p></div>`)
+                res.write(`<div<p><a href="/signup">Sign Up</a></p></div>`)
+                res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                res.end()
+            } else {
+                crypto.randomBytes(64, (err, buf) => {
+                    if (err) throw err;
+                    let salt = buf.toString('base64')
+                    crypto.pbkdf2(password, salt, Number(process.env.CRYPTO_ITERATION), 64, 'sha512', (err, key) => {
+                        derivedKey = key.toString('base64')
+                        db.query(`INSERT INTO Student (NumId, Name, UserId, Password, created, salt) VALUES (?, ?, ?, ?, NOW(), ?)`, [numId, name, userId, derivedKey, salt], (err, result) => {
+                            if (err) throw err
+                            res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                            res.write(`<p>${name}(${userId})님, 회원가입을 환영합니다!</p>`)
+                            res.write(`<p><a href="/">HOME</a></p>`)
+                            res.write(`<p><a href="login">Login</a></p>`)
+                            res.end()
+                        })
+                    })
+                })
+            }
         })
     } else if (status === 'faculty') {
-        db.query(`INSERT INTO 교수 (사번, 이름, 아이디, 비밀번호, created) VALUES (?, ?, ?, ?, NOW())`, [numId, name, userId, password], (err, result) => {
-            if (err) throw err
-            res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
-            res.write(`<p>처리되었습니다.</p>`)
-            res.write(`<p><a href="/">HOME</a></p>`)
-            res.write(`<p><a href="login">Login</a></p>`)
-            res.end()
+        db.query(`SELECT * FROM Faculty where NumId=? || UserId=?`, [numId, userId], (err, results) => {
+            if (err) throw err;
+            if (results[0]) {       // 이미 존재하는 사번 또는 아이디
+                res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                res.write(`입력하신 사번 또는 아이디로 가입된 계정이 이미 존재합니다.`)
+                res.write(`<div><p><a href="/login">Login</a></p></div>`)
+                res.write(`<div<p><a href="/signup">Sign Up</a></p></div>`)
+                res.write(`<div><p><a href="/">HOME</a></p></div>`)
+                res.end()
+            } else {
+                crypto.randomBytes(64, (err, buf) => {
+                    if (err) throw err;
+                    let salt = buf.toString('base64')
+                    crypto.pbkdf2(password, salt, Number(process.env.CRYPTO_ITERATION), 64, 'sha512', (err, key) => {
+                        derivedKey = key.toString('base64')
+                        db.query(`INSERT INTO Faculty (NumId, Name, UserId, Password, created, salt) VALUES (?, ?, ?, ?, NOW(), ?)`, [numId, name, userId, derivedKey, salt], (err, result) => {
+                            if (err) throw err
+                            res.writeHead('200', { 'Content-Type' : 'text/html; charset=utf8'})
+                            res.write(`<p>${name}(${userId})님, 회원가입을 환영합니다!</p>`)
+                            res.write(`<p><a href="/">HOME</a></p>`)
+                            res.write(`<p><a href="login">Login</a></p>`)
+                            res.end()
+                        })
+                    })
+                })
+            }
         })
     }
 })
